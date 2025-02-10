8c15eee488fc3c49a83aebb4c3e24fb4f3bf2b4c
Uriel Phoenix <urielosirisphoenix@hotmail.co.uk>
2025-02-10 13:58:54 +0000

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Uriel Phoenix on 10/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
